46dcf167ab3a4259db61102bd6cd71edf9c1b233
poonasq257 <poonasp257@naver.com>
2019-05-24 19:38:37 +0900

    Update
    DirectSound 클래스 추가

--- Engine/systemclass.h
+++ Engine/systemclass.h
@@ -22,6 +22,7 @@
 ///////////////////////
 #include "inputclass.h"
 #include "graphicsclass.h"
+#include "soundClass.h"
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -52,6 +53,7 @@ private:
 
 	InputClass* m_Input;
 	GraphicsClass* m_Graphics;
+	SoundClass* m_Sound;
 };
 
 
